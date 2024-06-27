2b8054890cfe09c22c2f8ff548b7aa316d7f1da7
keketee <elgrankeko@hotmail.com>
2024-06-27 17:23:53 +0200

    update

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,37 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
+
+
+// index.js
+
+document.addEventListener("DOMContentLoaded", () => {
+    const passwordInput = document.getElementById("password");
+    const rangeInput = document.getElementById("range");
+    const lengthText = document.getElementById("length-text");
+    const generateButton = document.getElementById("generate-password");
+  
+    const generatePassword = (length) => {
+      const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~`|}{[]:;?><,./-=";
+      let password = "";
+      for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * charset.length);
+        password += charset[randomIndex];
+      }
+      return password;
+    };
+  
+    rangeInput.addEventListener("input", (event) => {
+      const length = event.target.value;
+      lengthText.textContent = length;
+    });
+  
+    generateButton.addEventListener("click", () => {
+      const length = rangeInput.value;
+      const newPassword = generatePassword(length);
+      passwordInput.value = newPassword;
+    });
+  
+    
+    passwordInput.value = generatePassword(rangeInput.value);
+  });
+  
